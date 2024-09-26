17de5da3854a4641cb3cab166d57e9e1a3761af4
Akhilesh mishra <akhileshmishrabiz@gmail.com>
2024-09-27 01:54:43 +0530

    flask app ecs infra

---
+++ infra/local.tf
@@ -0,0 +1,81 @@
+locals {
+  db_data = {
+    allocated_storage       = 30
+    max_allocated_storage   = 100
+    engine_version          = "14.10"
+    instance_class          = "db.t3.small"
+    ca_cert_name            = "rds-ca-2019"
+    backup_retention_period = 7
+    db_name                 = "mydb"
+    cloudwatch_logs         = ["postgresql", "upgrade"]
+  }
+
+  ecs_services = [
+    {
+      name          = "flask-app"
+      cpu           = 1024
+      memory        = 2048
+      template_file = "task-definitions/flask-service.json.tpl"
+      vars = {
+        aws_ecr_repository            = aws_ecr_repository.python_app.repository_url
+        tag                           = "latest"
+        container_name                = "flask-app"
+        aws_cloudwatch_log_group_name = "/aws/ecs/${var.environment}-flask-app"
+        database_address              = aws_db_instance.postgres.address
+        database_name                 = aws_db_instance.postgres.db_name
+        postgres_username             = aws_db_instance.postgres.username
+        postgres_password             = random_password.dbs_random_string.result
+        environment                   = var.environment
+      }
+    },
+    {
+      name          = "nginx"
+      cpu           = 1024
+      memory        = 2048
+      template_file = "task-definitions/nginx-service.json.tpl"
+      vars = {
+        aws_ecr_repository            = "366140438193.dkr.ecr.ap-south-1.amazonaws.com/nginx"
+        tag                           = "latest"
+        container_name                = "nginx"
+        aws_cloudwatch_log_group_name = "/aws/ecs/${var.environment}-nginx"
+        environment                   = var.environment
+      }
+    },
+    {
+      name          = "redis"
+      cpu           = 1024
+      memory        = 2048
+      template_file = "task-definitions/redis-service.json.tpl"
+      vars = {
+        aws_ecr_repository            = "redis"
+        tag                           = "latest"
+        container_name                = "redis"
+        aws_cloudwatch_log_group_name = "/aws/ecs/${var.environment}-redis"
+        environment                   = var.environment
+      }
+    }
+  ]
+
+  app_deploy_data ={
+  IMAGE_NAME: "flask-app"
+  ECR_REGISTRY: "366140438193.dkr.ecr.ap-south-1.amazonaws.com"
+  ECR_REPOSITORY: "${var.environment}-app"
+  ACCOUNT_ID: "366140438193"
+  ECS_CLUSTER: "${var.environment}-app-cluster"
+  ECS_REGION: "ap-south-1"
+  ECS_SERVICE: "${var.environment}-flask-app-service"
+  ECS_TASK_DEFINITION: "${var.environment}-flask-app"
+  ECS_APP_CONTAINER_NAME: "flask-app"
+  }
+}
+
+
+resource "aws_secretsmanager_secret" "app_deploy_data" {
+  name        = "${var.environment}-app-deploy-data"
+  description = "Deployment data for the Flask app"
+}
+
+resource "aws_secretsmanager_secret_version" "app_deploy_data_version" {
+  secret_id     = aws_secretsmanager_secret.app_deploy_data.id
+  secret_string = jsonencode(local.app_deploy_data)
+}
---
+++ infra/rds.tf
@@ -0,0 +1,103 @@
+resource "aws_kms_key" "rds_kms" {
+  description             = "KMS key for RDS and Secrets Manager"
+  deletion_window_in_days = 10
+
+  tags = {
+    Name        = "rds-kms-key"
+    Environment = var.environment
+  }
+}
+
+resource "aws_kms_alias" "rds_kms_alias" {
+  name          = "alias/rds-kms-key"
+  target_key_id = aws_kms_key.rds_kms.id
+}
+
+resource "aws_db_instance" "postgres" {
+  identifier            = "${var.environment}-${var.app_name}-db"
+  allocated_storage     = lookup(local.db_data, "allocated_storage", var.db_default_settings.allocated_storage)
+  max_allocated_storage = lookup(local.db_data, "max_allocated_storage", var.db_default_settings.max_allocated_storage)
+  engine                = data.aws_rds_engine_version.postgresql.engine
+  engine_version        = lookup(local.db_data, "engine_version", var.db_default_settings.engine_version)
+  instance_class        = lookup(local.db_data, "instance_class", var.db_default_settings.instance_class)
+  username              = "postgres"
+  password              = random_password.dbs_random_string.result
+  port                  = 5432
+  publicly_accessible   = false
+  db_subnet_group_name  = aws_db_subnet_group.postgres.id
+  # ca_cert_identifier    = lookup(local.db_data, "ca_cert_name", var.db_default_settings.ca_cert_name)
+  storage_encrypted = true
+  storage_type      = "gp3"
+  kms_key_id        = aws_kms_key.rds_kms.arn
+  vpc_security_group_ids = [
+    aws_security_group.rds.id
+  ]
+
+  backup_retention_period         = lookup(local.db_data, "backup_retention_period", var.db_default_settings.backup_retention_period)
+  db_name                         = lookup(local.db_data, "db_name", var.db_default_settings.db_name)
+  auto_minor_version_upgrade      = true
+  deletion_protection             = true
+  enabled_cloudwatch_logs_exports = lookup(local.db_data, "cloudwatch_logs", ["postgresql", "upgrade"])
+  copy_tags_to_snapshot           = true
+
+  tags = {
+    environment = var.environment
+  }
+}
+
+resource "aws_secretsmanager_secret" "dbs_secret" {
+  name                    = "db/${var.environment}"
+  description             = "DB link"
+  kms_key_id              = aws_kms_key.rds_kms.arn
+  recovery_window_in_days = 7
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_secretsmanager_secret_version" "dbs_secret_val" {
+  secret_id     = aws_secretsmanager_secret.dbs_secret.id
+  secret_string = "postgres://${var.db_default_settings.db_admin_username}:${random_password.dbs_random_string.result}@${aws_db_instance.postgres.address}:${aws_db_instance.postgres.port}/${aws_db_instance.postgres.db_name}"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "random_password" "dbs_random_string" {
+  length           = 10
+  special          = false
+  override_special = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+}
+
+resource "aws_security_group" "rds" {
+  name        = "rds-sg"
+  vpc_id      = aws_vpc.main.id
+  description = "allow inbound access from the ECS only"
+
+  ingress {
+    protocol        = "tcp"
+    from_port       = 5432
+    to_port         = 5432
+    cidr_blocks     = ["0.0.0.0/0"]
+    security_groups = [aws_security_group.ecs_tasks.id]
+  }
+
+  egress {
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_db_subnet_group" "postgres" {
+  name        = "${var.environment}-${var.app_name}-db-subnet-group"
+  description = "Subnet group for RDS instance"
+  subnet_ids  = aws_subnet.rds.*.id
+
+  tags = {
+    Name        = "${var.environment}-${var.app_name}-db-subnet-group"
+    Environment = var.environment
+  }
+}
